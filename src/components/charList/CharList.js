ca8ff2d56ff72aa25ec2043e416739d47a945fb5
IrynaKrt <rinakashi13@mail.ru>
2023-02-16 15:54:33 +0300

    fix: add refs for the char-list

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -77,31 +77,55 @@ class CharList extends Component {
         })
     }
 
-    // Этот метод создан для оптимизации,
-    // чтобы не помещать такую конструкцию в метод render
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref); //в крайних случаях!
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail.includes('image_not_available')) {
                 imgStyle = {'objectFit' : 'unset'};
             }
+
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
             )
         });
-        // А эта конструкция вынесена для центровки спиннера/ошибки
+
         return (
             <ul className="char__grid">
                 {items}
             </ul>
         )
     }
+
 
     render() {
 
